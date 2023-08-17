edf03dc5a391bc1d4bbf99b4f266cb8b44a53fd0
athowes <adamthowes@gmail.com>
2023-08-17 12:32:06 +0100

    Move forward HIV/AIDS, Bayesian, and multi-agyw chapters

--- figures/bayesian/conjugate.R
+++ figures/bayesian/conjugate.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
 
 a <- 3
@@ -8,20 +10,18 @@ set.seed(1)
 truth <- 2.5
 y <- rpois(3, lambda = truth)
 
-prior <- ggplot(data = data.frame(x = c(0, 8)), aes(x)) +
-  stat_function(fun = dgamma, n = 500, args = list(shape = a, rate = b), col = "#56B4E9") +
-  annotate("text", x = 5.5, y = 0.2, label = "Prior: Gamma(3, 1)", col = "#56B4E9", size = 4) +
-  labs(x = "", y = "") +
+ggplot(data = data.frame(x = c(0, 8)), aes(x)) +
+  geom_function(aes(col = "Prior"), fun = dgamma, n = 500, args = list(shape = a, rate = b)) +
+  geom_function(aes(col = "Posterior"), fun = dgamma, n = 500, args = list(shape = a + sum(y), rate = b + length(y))) +
+  geom_vline(aes(col = "Truth", xintercept = truth)) +
+  scale_color_manual(values = cbpalette) +
+  geom_point(data = data.frame(x = y, y = 0), aes(x = x, y = y), inherit.aes = FALSE, alpha = 0.7, size = 2) +
+  geom_col(data = data.frame(x = c(0, 0, 0), y = c(0, 0, 0), type = c("Posterior", "Prior", "Truth")), aes(x = x, y = y, fill = type)) +
+  scale_fill_manual(values = cbpalette) +
+  labs(x = "", y = "", col = "", fill = "") +
+  guides(col = FALSE, fill = guide_legend(override.aes = list(alpha = 1, shape = 15))) +
   theme_minimal()
 
-3 + sum(y)
-
-
-prior +
-  geom_point(data = data.frame(x = y, y = 0), aes(x = x, y = y), inherit.aes = FALSE, alpha = 0.7, size = 2) +
-  stat_function(data = data.frame(x = c(0, 8)), aes(x), fun = dgamma, n = 500, args = list(shape = a + sum(y), rate = b + length(y)), col = "#009E73") +
-  annotate("text", x = 5, y = 0.3, label = "Posterior: Gamma(9, 4)", col = "#009E73", size = 4) +
-  geom_vline(xintercept = truth, col = "#E69F00", linetype = "dashed") +
-  annotate("text", x = 3.5, y = 0.4, label = "Truth", col = "#E69F00", size = 4)
-
 ggsave("figures/bayesian/conjugate.png", h = 3, w = 6.25, bg = "white")
+
+3 + sum(y)
