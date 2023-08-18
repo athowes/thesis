5de7a97937298001bfd635f4faccd212647f4147
athowes <adamthowes@gmail.com>
2023-08-18 15:58:49 +0100

    Render for Phil

--- figures/bayesian/hierarchical-structure.R
+++ figures/bayesian/hierarchical-structure.R
@@ -1,3 +1,15 @@
 setwd(here::here("figures/bayesian"))
 tools::texi2dvi("hierarchical-structure.tex", pdf = TRUE, clean = TRUE)
+
+convert_pdf_png <- function(name, dpi = 300) {
+  command <- paste0(
+    "convert -density ", dpi, " ", name, ".pdf -scene 1 -background white",
+    " -alpha remove -alpha off -quality 90 ", name, ".png"
+  )
+  system(command)
+}
+
+convert_pdf_png(name = "hierarchical-structure", dpi = 300)
+
 setwd(".")
+
--- figures/introduction/chapter-flowchart.R
+++ figures/introduction/chapter-flowchart.R
@@ -1,3 +1,14 @@
 setwd(here::here("figures/introduction/"))
 tools::texi2dvi("chapter-flowchart.tex", pdf = TRUE, clean = TRUE)
+
+convert_pdf_png <- function(name, dpi = 300) {
+  command <- paste0(
+    "convert -density ", dpi, " ", name, ".pdf -scene 1 -background white",
+    " -alpha remove -alpha off -quality 90 ", name, ".png"
+  )
+  system(command)
+}
+
+convert_pdf_png(name = "chapter-flowchart", dpi = 300)
+
 setwd(".")
