caf2f2f45fd761ca87ac7f338b48941633df31b0
athowes <adamthowes@gmail.com>
2023-09-06 17:06:52 +0100

    Easy Jeff fixes

--- figures/bayesian/st.R
+++ figures/bayesian/st.R
@@ -9,7 +9,6 @@ ct <- data.frame(name = "Cape Town", x = 18.375, y = -33.95) %>%
   sf::st_as_sf(coords = c("x", "y"), crs = "WGS84")
 
 sf_lightgrey <- "#E6E6E6"
-darkgrey <- "#4A4A4A"
 
 i <- 9
 indicator <- c(rep(0, i - 1), 1, rep(0, nrow(sf) - i))
@@ -17,12 +16,12 @@ indicator <- c(rep(0, i - 1), 1, rep(0, nrow(sf) - i))
 space <- ggplot() +
   geom_sf(data = sf, aes(fill = as.factor(indicator))) +
   annotate("text", x = 15, y = -33.95, label = "Cape Town", size = 3) +
-  geom_sf(data = ct, aes(geometry = geometry), size = 2, col = darkgrey) +
+  geom_sf(data = ct, aes(geometry = geometry), size = 2, col = "#009E73") +
   annotate("text", x = 20, y = -23.5, label = "ZF Mgcawu\nDistrict Municipality", size = 3) +
   labs(x = "", y = "", fill = "") +
   lims(x = c(13, NA)) +
-  scale_fill_manual(values = c(sf_lightgrey, darkgrey)) +
-  theme_minimal() +
+  scale_fill_manual(values = c(sf_lightgrey, "#56B4E9")) +
+  theme_void() +
   theme(legend.position = "none")
 
 time <- data.frame(
@@ -31,17 +30,19 @@ time <- data.frame(
     alpha = c(0, 1)
   ) %>%
   ggplot(aes(x = x, y = as.factor(y), alpha = alpha)) +
-  geom_point(col = darkgrey, size = 2) +
-  geom_segment(aes(x = 4, y = 2, xend = 7, yend = 2), col = darkgrey, size = 1, arrow = arrow(ends = "both", length = unit(0.1, "inches"))) +
+  geom_point(col = "#009E73", size = 2) +
+  geom_segment(aes(x = 4, y = 2, xend = 7, yend = 2), col = "#56B4E9", linewidth = 0.5, arrow = arrow(ends = "both", length = unit(0.1, "inches"))) +
   scale_x_continuous(breaks = 1:12, labels = month.abb, limits = c(1, 12)) +
-  scale_y_discrete(labels = c("World AIDS Day", "Q2")) +
-  coord_flip() +
+  scale_y_discrete(labels = c("World\nAIDS\nDay", "Q2")) +
   scale_alpha(range = c(0, 1)) +
   guides(alpha = "none") +
   labs(x = "", y = "") +
   theme_minimal() +
-  theme(panel.grid.minor = element_blank())
+  theme(
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+  )
 
-space + time + plot_layout(widths = c(1.25, 1))
+space + time + plot_layout(widths = c(1.15, 1))
 
 ggsave("figures/bayesian/st.png", h = 2.5, w = 6.25)
