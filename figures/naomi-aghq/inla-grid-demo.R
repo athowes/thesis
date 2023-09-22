e916984e2343fa03a95e067192876bb172cefc0b
athowes <adamthowes@gmail.com>
2023-09-22 16:44:44 +0100

    R-INLA grid method demonstration

--- figures/naomi-aghq/inla-grid-demo.R
+++ figures/naomi-aghq/inla-grid-demo.R
@@ -45,6 +45,99 @@ fig0 <- ggplot(ground_truth, aes(x = theta1, y = theta2, z = pdf)) +
     axis.text.y = element_blank(), axis.ticks.y = element_blank()
   )
 
-fig0 + labs(tag = "A") + fig0 + labs(tag = "B")
+#' R-INLA grid method
+Lambda <- diag(eigen(cov)$values)
+E <- eigen(cov)$vectors
+E %*% Lambda %*% t(E)
+
+z_to_theta <- function(z) {
+  as.vector(opt$par + E %*% sqrt(Lambda) %*% z)
+}
+
+theta_mode <- z_to_theta(c(0, 0))
+theta_mode
+
+test_statistic <- function(theta_proposal) {
+  obj$fn(theta_proposal) - obj$fn(theta_mode)
+}
+
+#' @param j is the index of exploration direction
+#' @param m is dim(theta)
+#' @param delta_z is the step size
+#' @param delta_pi is the acceptable drop-off
+explore_direction <- function(j, m, delta_z, delta_pi) {
+  z_mode <- rep(0, m)
+  z_names <- paste0("z", 1:m)
+  names(z_mode) <- z_names
+  
+  unit_vector <- rep(0, m)
+  unit_vector[j] <- 1
+  
+  points <- rbind(z_mode)
+  
+  # Increasing
+  i <- 0
+  condition <- TRUE
+  while(condition) {
+    i <- i + 1
+    proposal <- c(z_mode + i * delta_z %*% unit_vector)
+    names(proposal) <- z_names
+    statistic <- test_statistic(z_to_theta(proposal))
+    condition <- (statistic < delta_pi)
+    if(condition){
+      points <- rbind(points, proposal)
+    }
+  }
+  
+  # Decreasing
+  i <- 0
+  condition <- TRUE
+  while(condition) {
+    i <- i + 1
+    proposal <- c(z_mode - i * delta_z %*% unit_vector)
+    names(proposal) <- z_names
+    statistic <- test_statistic(z_to_theta(proposal))
+    condition <- (statistic < delta_pi)
+    if(condition){
+      points <- rbind(points, proposal)
+    }
+  }
+  as.data.frame(points)
+}
+
+d_z <- 0.5
+d_pi <- 2
+
+z_grid <- expand.grid(
+  z1 = explore_direction(1, 2, delta_z = d_z, delta_pi = d_pi)$z1,
+  z2 = explore_direction(2, 2, delta_z = d_z, delta_pi = d_pi)$z2
+)
+
+theta_grid_full <- t(apply(z_grid, 1, z_to_theta)) %>%
+  as.data.frame() %>%
+  rename(theta1 = V1, theta2 = V2)
+
+theta_grid <- theta_grid_full %>%
+  mutate(
+    statistic = apply(theta_grid_full, 1, test_statistic),
+    condition = statistic < d_pi
+  ) %>%
+  filter(condition == TRUE)
+
+fig1 <- fig0 +
+  geom_point(
+    data = theta_grid,
+    aes(x = theta1, y = theta2, size = 1),
+    alpha = 0.8,
+    col = "#009E73",
+    inherit.aes = FALSE
+  ) +
+  scale_size_continuous(range = c(1, 2)) +
+  labs(tag = "A") +
+  theme(
+    plot.caption = element_text(hjust = 0.5, vjust = 1)
+  )
+
+fig1 + fig0 + labs(tag = "B")
 
 ggsave("figures/naomi-aghq/inla-grid-demo.png", h = 3.5, w = 6.25)
