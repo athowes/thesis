5affb154894ce27da1ca551e87bc29623909c6a5
athowes <adamthowes@gmail.com>
2023-10-12 12:00:41 +0100

    Lunchtime check-in

--- figures/naomi-aghq/epil.R
+++ figures/naomi-aghq/epil.R
@@ -7,10 +7,10 @@ data("Epil")
 Epil %>%
   ggplot(aes(x =  as.factor(Trt), y = y)) +
   geom_boxplot(outlier.shape = NA) +
-  geom_jitter(aes(col = as.factor(V4)), width = 0.2, alpha = 0.7, size = 2, shape = 1) +
-  scale_colour_manual(values = c("grey", "#56B4E9")) +
+  geom_jitter(width = 0.2, alpha = 0.7, size = 2, shape = 1) +
+  # scale_colour_manual(values = c("grey", "#56B4E9")) +
   coord_flip() +
-  labs(x = "Treatment", y = "Number of seizures", col = "Final visit?") +
+  labs(x = "Treatment", y = "Number of seizures") +
   theme_minimal()
 
 ggsave("figures/naomi-aghq/epil.png", h = 3.5, w = 6.25, bg = "white")
