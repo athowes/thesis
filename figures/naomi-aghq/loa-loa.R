4dd3b149577d4a8f526c0e36125648584efb1f3c
athowes <adamthowes@gmail.com>
2023-11-08 16:38:22 +0000

    Progress on conditional simulation Loa loa example

--- figures/naomi-aghq/loa-loa.R
+++ figures/naomi-aghq/loa-loa.R
@@ -8,9 +8,6 @@ data(loaloa, package = "geostatsp")
 loaloa <- terra::vect(loaloa)
 loaloa_sf <- sf::st_as_sf(loaloa)
 
-# Set the resolution for the spatial interpolations
-reslist <- list(nrow = 50, ncol = 100)
-
 compile("resources/naomi-aghq/loaloazip.cpp")
 dyn.load(dynlib("resources/naomi-aghq/loaloazip"))
 
@@ -30,24 +27,24 @@ sigma_alpha <- 0.025
 rho_u <- 2e05
 rho_alpha <- 0.975
 
-maternconstants <- list()
-maternconstants$d <- 2
-maternconstants$nu <- 1
+matern <- list()
+matern$d <- 2
+matern$nu <- 1
 
 get_kappa <- function(sigma, rho) {
-  sqrt(8 * maternconstants$nu) / rho
+  sqrt(8 * matern$nu) / rho
 }
 
 get_tau <- function(sigma, rho) {
-  sigma * get_kappa(sigma, rho)^(maternconstants$nu) * sqrt(gamma(maternconstants$nu + maternconstants$d / 2) * (4 * pi)^(maternconstants$d / 2) / gamma(maternconstants$nu))
+  sigma * get_kappa(sigma, rho)^(matern$nu) * sqrt(gamma(matern$nu + matern$d / 2) * (4 * pi)^(matern$d / 2) / gamma(matern$nu))
 }
 
 get_sigma <- function(kappa, tau) {
-  tau / (kappa^(maternconstants$nu) * sqrt(gamma(maternconstants$nu + maternconstants$d / 2) * (4 * pi)^(maternconstants$d / 2) / gamma(maternconstants$nu)))
+  tau / (kappa^(matern$nu) * sqrt(gamma(matern$nu + matern$d / 2) * (4 * pi)^(matern$d / 2) / gamma(matern$nu)))
 }
 
 get_rho <- function(kappa, tau) {
-  sqrt(8 * maternconstants$nu) / kappa
+  sqrt(8 * matern$nu) / kappa
 }
 
 beta_prec <- 0.001
@@ -56,7 +53,7 @@ dat <- list(
   y = y,
   N = N,
   design = design,
-  nu = maternconstants$nu,
+  nu = matern$nu,
   rho_u = rho_u,
   rho_alpha = rho_alpha,
   sigma_u = sigma_u,
@@ -85,7 +82,7 @@ obj <- MakeADFun(
   silent = TRUE
 )
 
-quad <- aghq::marginal_laplace_tmb(obj, 1, startingvalue = c(param$logkappa, param$logtau))
+quad <- aghq::marginal_laplace_tmb(obj, 3, startingvalue = c(param$logkappa, param$logtau))
 
 # Sampling from AGHQ fitted model
 
@@ -98,7 +95,6 @@ loaloa_sf <- mutate(loaloa_sf, p = y / N)
 grid <- raster::raster(loaloa_sf, nrows = 30, ncols = 30)
 grid_stars <- stars::st_as_stars(grid)
 b <- gstat::krige(p ~ 1, loaloa_sf, grid_stars)
-b <- gstat::krige(p ~ 1, loaloa_sf, grid_stars, nsim = 1)
 b_sf <- st_as_sf(b)
 
 ggplot() +
@@ -112,27 +108,47 @@ ggplot() +
 
 ggsave("figures/naomi-aghq/loa-loa-data.png", h = 4, w = 6.25)
 
-# loazippostsamples <- sample_marginal(quad, 100)
-# postU <- loazippostsamples$samps[c(1:190), ]
-# postV <- loazippostsamples$samps[c(192:381), ]
-# postBeta <- loazippostsamples$samps[c(191, 382), ]
-# 
-# U <- postU  
-# theta <- loazippostsamples$theta
-# pointsdata <- loaloa_sf
-# resolution <- reslist
-# 
-# modpar <- cbind(
-#   var = get_sigma(exp(theta$logkappa), exp(theta$logtau))^2,
-#   range = get_rho(exp(theta$logkappa), exp(theta$logtau)),
-#   shape = maternconstants$nu
-# )
-# 
-# fielddat <- pointsdata
-# fielddat@data <- as.data.frame(U)
-# 
-# geostatsp::RFsimulate(
-#   model = modpar,
-#   data = fielddat,
-#   x = raster::raster(fielddat, nrows = resolution$nrow, ncols = resolution$ncol)
-# )
+aghq_samples <- sample_marginal(quad, 100)
+u_samples <- aghq_samples$samps[c(1:190), ]
+v_samples <- aghq_samples$samps[c(192:381), ]
+beta_samples <- aghq_samples$samps[c(191, 382), ]
+
+theta_samples <- aghq_samples$theta
+
+covariance_samples <- cbind(
+  var = get_sigma(exp(theta_samples$logkappa), exp(theta_samples$logtau))^2,
+  range = get_rho(exp(theta_samples$logkappa), exp(theta_samples$logtau)),
+  shape = matern$nu
+)
+
+# Resolution for spatial interpolations
+resolution <- list(nrow = 50, ncol = 50)
+
+options(useRandomFields = TRUE)
+
+loaloa_u <- vect(loaloa_sf$geometry)
+values(loaloa_u) <- as.data.frame(u_samples)
+
+u_brick <- geostatsp::RFsimulate(
+  model = covariance_samples,
+  x = terra::rast(loaloa, nrows = resolution$nrow, ncols = resolution$ncol),
+  data = loaloa_u
+)
+
+nrow(covariance_samples)
+
+# Error in UnifyXT(x, y, z, T, grid = grid, distances = distances, dim = dim) : 
+#   is.numeric(x) is not TRUE
+# model="matrix", x="SpatVector"
+
+loaloa_v <- vect(loaloa_sf$geometry)
+values(loaloa_v) <- as.data.frame(v_samples)
+
+v_brick <- geostatsp::RFsimulate(
+  model = covariance_samples,
+  x = terra::rast(loaloa, nrows = resolution$nrow, ncols = resolution$ncol),
+  data = loaloa_v
+)
+
+eta_phi_brick <- beta_samples[1, ] + u_brick
+eta_rho_brick <- beta_samples[2, ] + v_brick
