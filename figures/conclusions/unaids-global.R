c73a961b07a5a44883c805cbb50448b3256b4f0b
athowes <adamthowes@gmail.com>
2023-12-24 10:10:03 +0000

    Add global report figure, move k = 7 into conclusions and add caption

---
+++ figures/conclusions/unaids-global.R
@@ -0,0 +1,23 @@
+library(pdftools)
+library(magick)
+library(ggplot2)
+library(patchwork)
+
+pdf_file <- "figures/conclusions/global-update.pdf"
+
+title_page <- pdftools::pdf_convert(pdf_file, pages = 1, filenames = "figures/conclusions/title.png", dpi = 450)
+figure_page <- pdftools::pdf_convert(pdf_file, pages = 53, filenames = "figures/conclusions/figure.png", dpi = 450)
+
+title_ggplot <- ggplot() +
+  annotation_custom(grid::rasterGrob(image_read(title_page)), xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
+  theme_void() +
+  labs(tag = "A")
+
+figure_ggplot <- ggplot() +
+  annotation_custom(grid::rasterGrob(image_read(figure_page)), xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
+  theme_void() +
+  labs(tag = "B")
+
+title_ggplot + figure_ggplot + plot_layout(ncol = 2)
+
+ggsave("figures/conclusions/global-update.png", height = 4.5, width = 6.25, dpi = 450)
