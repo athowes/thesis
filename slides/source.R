6aa48267e119279d3902e296b143a48b09d9df27
athowes <adamthowes@gmail.com>
2024-02-22 11:34:20 +0000

    Working towards HIV group presentation slides

--- slides/source.R
+++ slides/source.R
@@ -1,4 +1,5 @@
 rmarkdown::render(input = "slides/slides.Rmd", output_dir = "docs", output_format = "beamer_presentation")
+rmarkdown::render(input = "slides/slides-long.Rmd", output_dir = "docs", output_format = "beamer_presentation")
 
 png_file <- paste0("docs/slide-", 1, ".png")
 pdftools::pdf_convert("docs/slides.pdf", pages = 1, format = "png", filenames = png_file, dpi = 300)
