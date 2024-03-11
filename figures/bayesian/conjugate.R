8e8bc559595973b8ff7ae8158b0022541633ea06
athowes <adamthowes@gmail.com>
2024-03-11 15:39:55 +0000

    Fixes to 3-4 Chris P minor corrections #9

--- figures/bayesian/conjugate.R
+++ figures/bayesian/conjugate.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
+cbpalette <- c("#56B4E9","#009E73", "#E69F00", "#CC79A7")
 
 a <- 3
 b <- 1
@@ -10,13 +10,17 @@ set.seed(1)
 truth <- 2.5
 y <- rpois(3, lambda = truth)
 
+types <- c("Prior", "Likelihood", "Posterior", "Truth")
+types <- factor(types, levels = unique(types))
+
 ggplot(data = data.frame(x = c(0, 8)), aes(x)) +
-  geom_function(aes(col = "Prior"), fun = dgamma, n = 500, args = list(shape = a, rate = b)) +
-  geom_function(aes(col = "Posterior"), fun = dgamma, n = 500, args = list(shape = a + sum(y), rate = b + length(y))) +
-  geom_vline(aes(col = "Truth", xintercept = truth)) +
+  geom_function(aes(col = "1"), fun = dgamma, n = 500, args = list(shape = a, rate = b)) +
+  geom_function(aes(col = "2"), fun = dgamma, n = 500, args = list(shape = sum(y), rate = length(y))) +
+  geom_function(aes(col = "3"), fun = dgamma, n = 500, args = list(shape = a + sum(y), rate = b + length(y))) +
+  geom_vline(aes(col = "4", xintercept = truth)) +
   scale_color_manual(values = cbpalette) +
   geom_point(data = data.frame(x = y, y = 0), aes(x = x, y = y), inherit.aes = FALSE, alpha = 0.7, size = 2, shape = 1) +
-  geom_col(data = data.frame(x = c(0, 0, 0), y = c(0, 0, 0), type = c("Posterior", "Prior", "Truth")), aes(x = x, y = y, fill = type)) +
+  geom_col(data = data.frame(x = c(0, 0, 0, 0), y = c(0, 0, 0, 0), type = types), aes(x = x, y = y, fill = type)) +
   scale_fill_manual(values = cbpalette) +
   labs(x = "", y = "", col = "", fill = "") +
   guides(col = "none", fill = guide_legend(override.aes = list(alpha = 1, shape = 15))) +
